545506a8ddae9c7b03933c4f244d5e0d45c22ca2
Adrian Dominguez <adriandgzg@gmail.com>
2022-03-08 14:21:09 -0600

    Initial Commit

---
+++ BanRegioTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BanRegioTest
+//
+//  Created by Adrian Dominguez on 08/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
